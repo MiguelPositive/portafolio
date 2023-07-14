bf347845806f8c8a4d9784d2d3d10461342208b5
Miguel-Angel <miguelpositive@gmail.com>
2023-07-13 20:35:24 -0500

    feat (app): creo las rutas publicas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Portafolio from "./views/Portafolio.jsx";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Portafolio /> },
+  { path: "*", element: <Portafolio /> },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
